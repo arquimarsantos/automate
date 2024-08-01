ea0c633e8e7e2007400f2599b1d871f7c75a8e8b
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-01 14:05:14 -0300

    add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from flask import Flask, request
-import os
 import time
 import random
 from datetime import datetime
@@ -15,13 +16,12 @@ def automate():
     while True:
         try:
             options = webdriver.ChromeOptions()
-            options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
             options.add_argument("--disable-blink-features=AutomationControlled")
             options.add_experimental_option("excludeSwitches", ["enable-automation"])
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
-            options.add_argument("--headless")
-            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+            options.add_argument("--headless=new")
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
             dtime = datetime.now()
             dt_string = dtime.strftime("%d/%m/%Y %H:%M:%S")
             groupNames = ['AMISTADES & STICKERS ENTREN', 'ENTRA BB', 'ENTREN GUAPOS', 'ENTRA AMOR TE ESPERO', 'VIRTUALITOS']
