69d98188142b46b59ce52465f0c300393ad5fbcf
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-01 06:44:03 -0300

    add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.common.service import Service
-from webdriver-manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
