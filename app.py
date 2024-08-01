9ffde3186810b5bb2d5c85ece2d2487ec959d012
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-01 16:25:57 -0300

    add files via upload

--- app.py
+++ app.py
@@ -11,20 +11,7 @@ import time
 import random
 from datetime import datetime
 
-app = Flask(__name__)
-
-from selenium import webdriver 
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities 
-from selenium.webdriver.chrome.options import Options
- d = DesiredCapabilities.CHROME
-  d['loggingPrefs'] = { 'performance':'ALL' } 
-  chrome_options = Options()
-   chrome_options.add_experimental_option('w3c', False)
-   driver = webdriver.Chrome(desired_capabilities=d, options=chrome_options)
-   
-chrome_options.add_argument("--window-size=1920,1080") chrome_options.add_argument("--disable-background-timer-throttling") chrome_options.add_argument("--disable-backgrounding-occluded-windows") chrome_options.add_argument("--disable-breakpad") chrome_options.add_argument("--disable-component-extensions-with-background-pages") chrome_options.add_argument("--disable-dev-shm-usage") chrome_options.add_argument("--disable-extensions") chrome_options.add_argument("--disable-features=TranslateUI,BlinkGenPropertyTrees") chrome_options.add_argument("--disable-ipc-flooding-protection") chrome_options.add_argument("--disable-renderer-backgrounding") chrome_options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess") chrome_options.add_argument("--force-color-profile=srgb") chrome_options.add_argument("--hide-scrollbars") chrome_options.add_argument("--metrics-recording-only") chrome_options.add_argument("--mute-audio") chrome_options.add_argument("--headless") chrome_options.add_argument("--no-sandbox")   
-   
-   
+app = Flask(__name__)   
 
 def automate():
     while True:
@@ -40,7 +27,8 @@ def automate():
             options.add_argument("--disable-ipc-flooding-protection")
             options.add_argument("--disable-renderer-backgrounding")
             options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
-            options.add_argument("--force-color-profile=srgb") chrome_options.add_argument("--hide-scrollbars")
+            options.add_argument("--force-color-profile=srgb")
+            options.add_argument("--hide-scrollbars")
             options.add_argument("--metrics-recording-only")
             options.add_argument("--mute-audio")
             options.add_argument("--disable-blink-features=AutomationControlled")
