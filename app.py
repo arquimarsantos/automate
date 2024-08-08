7a329ede8a68f88b54652f3277d9ce6a3cf3ac54
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-08 20:37:14 -0300

    add files via upload

--- app.py
+++ app.py
@@ -18,12 +18,16 @@ def automate():
     while True:
         try:
             options = webdriver.ChromeOptions()
+            user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36' 
+            options.add_argument(f'user-agent={user_agent}')
             options.add_argument("--disable-blink-features=AutomationControlled")
             options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            options.add_argument('--ignore-certificate-errors')
+            options.add_argument('--allow-running-insecure-content')
             options.add_argument('--disable-gpu')
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
-            options.add_argument("--headless")
+            options.add_argument("--headless=new")
             options.add_argument("--start-maximized")
             options.add_argument("--window-size=1920,1080")
             driver = webdriver.Chrome(options=options)
