24578a12ea00a37edb94f051613e700ea86ad376
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-08 21:13:45 -0300

    add files via upload

--- app.py
+++ app.py
@@ -39,7 +39,6 @@ def automate():
             driver.find_element("xpath", '//*[@id="btnpublica"]').click()
             driver.find_element("xpath", '//*[@id="myDiv3"]/span[6]').click()
             driver.find_element("xpath", '/html/body/div[2]/div/div[1]/button').click()
-            driver.find_element("xpath", '//*[@id="mailgrupo"]').click()
             driver.find_element("xpath", '//*[@id="mailgrupo"]').send_keys(email)
             driver.find_element("xpath", '//*[@id="FRMgest"]/button').click()
             time.sleep(5)
