37801bd82dcd28bd4f6d89fbb187a45c258588d9
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-15 16:24:06 -0300

    add files via upload

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def automate():
         driver.find_element(By.XPATH, '//*[@id="cat3"]/option[4]').click()
         driver.find_element(By.XPATH, '//*[@id="privacidad"]').click()
         driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[11]/div/a').click()
-        time.sleep(20)
+        driver.implicitly_wait(10)
         driver.find_element(By.XPATH, '//*[@id="frmALTA2"]/button[1]').click()
         time.sleep(5)
         driver.quit()
