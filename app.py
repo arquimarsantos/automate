005f82cbf3ef229261e0ade33ab064cd42978b85
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-21 02:13:12 -0300

    add files via upload

--- app.py
+++ app.py
@@ -19,13 +19,13 @@ group_link = "https://chat.whatsapp.com/IiCl4YhSt1qHduNAW7IWWZ"
 def automate():
     try:
         options = webdriver.ChromeOptions()
-        #user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36' 
-        #options.add_argument(f'user-agent={user_agent}')
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36' 
+        options.add_argument(f'user-agent={user_agent}')
         #options.add_argument(f"--proxy-server={proxy}")
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_argument('--ignore-certificate-errors')
-        options.add_argument('--allow-running-insecure-content')
+        #options.add_argument("--disable-blink-features=AutomationControlled")
+        #options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        #options.add_argument('--ignore-certificate-errors')
+        #options.add_argument('--allow-running-insecure-content')
         options.add_argument('--disable-gpu')
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
