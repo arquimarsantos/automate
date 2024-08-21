c20a592c3543d2f0499ee8ee3179670fd0a1a7cc
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-21 15:08:23 -0300

    add files via upload

--- app.py
+++ app.py
@@ -19,9 +19,12 @@ group_link = "https://chat.whatsapp.com/KbrxPxeqIDCHUUdYUoPJMG"
 def automate():
     try:
         options = webdriver.ChromeOptions()
-        #user_agent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36'
-        mobile_emulation = { "deviceName": "Nexus 5" }
-        #options.add_argument(f'user-agent={user_agent}')
+        #user_agent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36'
+        mobile_emulation = {
+   "deviceMetrics": { "width": 360, "height": 640, "pixelRatio": 3.0 },
+   "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19",
+   "clientHints": {"platform": "Android", "mobile": True} }
+        options.add_argument(f'user-agent={user_agent}')
         #options.add_argument(f"--proxy-server={proxy}")
         #options.add_argument("--disable-blink-features=AutomationControlled")
         #options.add_experimental_option("excludeSwitches", ["enable-automation"])
