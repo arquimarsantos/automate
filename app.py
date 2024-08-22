d23b2acef84a211bd669853df18f9a10d8963e0d
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-22 13:44:59 -0300

    add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from flask import Flask
 from imap_tools import MailBox
