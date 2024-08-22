f459c1a7032039e83543a2865e7f03ba054b9ac0
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-21 23:27:20 -0300

    add files via upload

--- app.py
+++ app.py
@@ -20,10 +20,7 @@ def automate():
     try:
         options = webdriver.ChromeOptions()
         #user_agent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36'
-        mobile_emulation = {
-   "deviceMetrics": { "width": 360, "height": 640, "pixelRatio": 3.0 },
-   "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19",
-   "clientHints": {"platform": "Android", "mobile": True} }
+        mobile_emulation = { "deviceName": "Nexus 7" }
         #options.add_argument(f'user-agent={user_agent}')
         #options.add_argument(f"--proxy-server={proxy}")
         #options.add_argument("--disable-blink-features=AutomationControlled")
