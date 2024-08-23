1fbd54f431627d1262bd75e72fb70b3bffa8a480
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-23 12:04:23 -0300

    add files via upload

--- app.py
+++ app.py
@@ -22,10 +22,14 @@ def automate():
     try:
         options = webdriver.ChromeOptions()
         mobile_emulation = { "deviceName": "iPhone X" }
+        user_agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
         #options.add_argument(f"--proxy-server={proxy}")
         #options.add_argument("--disable-blink-features=AutomationControlled")
         #options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option("mobileEmulation", mobile_emulation)
+        options.add_extension('urban.crx')
+        options.add_argument(f'user-agent={user_agent}')
+        Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('--allow-running-insecure-content')
         options.add_argument('--disable-gpu')
@@ -39,16 +43,35 @@ def automate():
         dt_string = dtime.strftime("%d/%m/%Y %H:%M:%S")
         names = random.choice(group_names)
         print("Automação iniciada! - ", dt_string)
+        driver.get('chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/popup/index.html#/welcome-consent')
+        time.sleep(5)
+        driver.switch_to.window(driver.window_handles[-1])
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+        time.sleep(5)
+        driver.find_element(By.XPATH,'//*[@id="app"]/div/div[1]/div[2]/div/div/div[2]/button[2]').click()
+        time.sleep(5)
+        driver.find_element(By.XPATH,'//*[@id="app"]/div/div[1]/div[2]/div/div[2]/button[2]').click()
+        driver.get('chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/popup/index.html')
+        time.sleep(10)
+        driver.find_element(By.XPATH,'//*[@id="app"]/div/div[1]/div/div[2]/div[2]/div[1]/div[1]/input').click()
+        time.sleep(5)
+        br_button = driver.find_element(By.XPATH,'//*[@id="app"]/div/div[1]/div/div[2]/div[2]/div[1]/div[2]/div/ul[2]/li[8]/p')
+        time.sleep(5)
+        ActionChains(driver).move_to_element(br_button).click(br_button).perform()
+        time.sleep(5)
         driver.get("https://www.gruposwats.com")
         try:
             cookies = pickle.load(open("cookies.pkl", "rb"))
             for cookie in cookies:
                 driver.add_cookie(cookie)
+                print("cookies salvos foram restaurados! - ", dt_string)
         except (OSError, IOError) as e:
             pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
+            print("criando novos cookies no banco de dados... - ", dt_string)
             
         driver.find_element(By.XPATH, '//*[@id="btnpublica"]').click()
-        driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[2]/input').send_keys("Amistades y más")
+        driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[2]/input').send_keys(names)
         driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[3]/input').send_keys(group_link)
         driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[5]/div/input[2]').click()
         driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[6]/div/input[1]').send_keys(email)
