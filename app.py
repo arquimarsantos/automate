54f7e29295c36822f2eaeefcee409a679c654778
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-22 23:48:41 -0300

    add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from flask import Flask
 from imap_tools import MailBox
@@ -20,6 +19,7 @@ group_link = "https://chat.whatsapp.com/KbrxPxeqIDCHUUdYUoPJMG"
 def automate():
     try:
         options = webdriver.ChromeOptions()
+        #user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'        
         mobile_emulation = { "deviceName": "iPhone X" }
         #options.add_argument(f"--proxy-server={proxy}")
         #options.add_argument("--disable-blink-features=AutomationControlled")
@@ -38,29 +38,65 @@ def automate():
         dt_string = dtime.strftime("%d/%m/%Y %H:%M:%S")
         names = random.choice(group_names)
         print("Automação iniciada! - ", dt_string)
-        driver.get("https://www.gruposwats.com")        
+        driver.get("https://www.gruposwats.com")
         driver.find_element(By.XPATH, '//*[@id="btnpublica"]').click()
-        driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[2]/input').send_keys("Amistades y más")
-        driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[3]/input').send_keys(group_link)
-        driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[5]/div/input[2]').click()
-        driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[6]/div/input[1]').send_keys(email)
-        driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[6]/div/input[2]').send_keys(email)
-        driver.find_element(By.XPATH, '//*[@id="descripcion"]').send_keys("Grupo para hacer amistades, parejas, stickers, y más... entra y desfruta!")
-        driver.find_element(By.XPATH, '//*[@id="keys"]').send_keys("amistad, amigos, humor, memes, ciudad, argentina, colombia, peru, perú, mexico, méxico")
-        driver.find_element(By.XPATH, '//*[@id="cat1"]').click()
-        driver.find_element(By.XPATH, '//*[@id="cat1"]/option[2]').click()
-        driver.find_element(By.XPATH, '//*[@id="cat2"]').click()
-        driver.find_element(By.XPATH, '//*[@id="cat2"]/option[3]').click()
-        driver.find_element(By.XPATH, '//*[@id="cat3"]').click()
-        driver.find_element(By.XPATH, '//*[@id="cat3"]/option[4]').click()
-        driver.find_element(By.XPATH, '//*[@id="privacidad"]').click()
-        driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[11]/div/a').click()
-        time.sleep(10)
-        driver.find_element(By.XPATH, '//*[@id="frmALTA2"]/a[1]').click()
+        driver.find_element(By.XPATH, '//*[@id="myDiv3"]/span[6]').click()
+        driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/button').click()
         time.sleep(5)
-        print(driver.get_cookies())
-        driver.quit()
-        print("Automação concluída com sucesso! - ", dt_string)
+        driver.find_element(By.XPATH, '//*[@id="mailgrupo"]').click()
+        driver.find_element(By.XPATH, '//*[@id="mailgrupo"]').send_keys(email)
+        driver.find_element(By.XPATH, '//*[@id="FRMgest"]/button').click()
+        time.sleep(10)
+        with MailBox('imap.gmail.com').login(email, password) as mailbox:
+            for msg in mailbox.fetch(limit=1, reverse=True, mark_seen=True):
+                uids = []
+                if (msg.from_ != "info@gruposwats.com"):
+                    print("Email selecionado não é válido com a automação, reiniciando sistema... - ", dt_string)
+                    return automate()
+                    
+                body = msg.text or msg.html
+                extractor = URLExtract()
+                url = extractor.find_urls(body)
+                first_url = url[0]
+                print("==================================================\n\nDe: ", msg.from_, "\nPara: ", msg.to, "\nAssunto: ", msg.subject, "\nData: ", msg.date, "\nUID: ", msg.uid, "\n\nMensagem: \n\n", body)
+                driver.get(first_url)
+                #time.sleep(10)
+                print(driver.get_cookies())
+                check = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div[1]/div/div[1]/span[5]')
+                state = "Estado: *** en revisión ***"
+                if state in check.text:
+                    uids.append(msg.uid)
+                    mailbox.delete(uids)
+                    driver.quit()
+                    return print("O grupo segue em revisão, por isso a automação será cancelada. - ", dt_string)
+                    
+                driver.find_element(By.XPATH, '//*[starts-with(@id,"btn")]/input').click()
+                time.sleep(5)
+                WebDriverWait(driver, 10).until(EC.alert_is_present())
+                driver.switch_to.alert.accept()
+                driver.find_element(By.XPATH, '//*[@id="btnpublica"]').click()
+                driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[2]/input').send_keys(names)
+                driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[3]/input').send_keys(group_link)
+                driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[5]/div/input[2]').click()
+                driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[6]/div/input[1]').send_keys(email)
+                driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[6]/div/input[2]').send_keys(email)
+                driver.find_element(By.XPATH, '//*[@id="descripcion"]').send_keys("Grupo para hacer amistades, parejas, stickers, y más... entra y desfruta!")
+                driver.find_element(By.XPATH, '//*[@id="keys"]').send_keys("amistad, amigos, humor, memes, ciudad, argentina, colombia, peru, perú, mexico, méxico")
+                driver.find_element(By.XPATH, '//*[@id="cat1"]').click()
+                driver.find_element(By.XPATH, '//*[@id="cat1"]/option[2]').click()
+                driver.find_element(By.XPATH, '//*[@id="cat2"]').click()
+                driver.find_element(By.XPATH, '//*[@id="cat2"]/option[3]').click()
+                driver.find_element(By.XPATH, '//*[@id="cat3"]').click()
+                driver.find_element(By.XPATH, '//*[@id="cat3"]/option[4]').click()
+                driver.find_element(By.XPATH, '//*[@id="privacidad"]').click()
+                driver.find_element(By.XPATH, '//*[@id="frmALTA1"]/div[11]/div/a').click()
+                time.sleep(10)
+                driver.find_element(By.XPATH, '//*[@id="frmALTA2"]/a[1]').click()
+                time.sleep(5)
+                uids.append(msg.uid)
+                mailbox.delete(uids)
+                driver.quit()
+                print("Automação concluída com sucesso! - ", dt_string)
     except Exception as e:
         print(e)
         
