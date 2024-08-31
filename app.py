6e2810f627e7a3a185e2949b9927162c13948e81
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-08-31 01:15:25 -0300

    add files via upload

--- app.py
+++ app.py
@@ -28,20 +28,20 @@ group_link = "https://chat.whatsapp.com/KbrxPxeqIDCHUUdYUoPJMG"
 def automate():
     try:
         options = webdriver.ChromeOptions()
-        mobile_emulation = { "deviceName": "iPhone X" }
+        #mobile_emulation = { "deviceName": "iPhone X" }
         #user_agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
         #options.add_argument(f'user-agent={user_agent}')
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option("mobileEmulation", mobile_emulation)
+        #options.add_experimental_option("mobileEmulation", mobile_emulation)
         options.add_argument('--allow-running-insecure-content')
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('--disable-gpu')
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--profile-directory=Default")
-        options.add_argument("--user-data-dir=selenium")
+        #options.add_argument("--profile-directory=Default")
+        #options.add_argument("--user-data-dir=selenium")
         options.add_argument("--headless=new")
         options.add_argument("--start-maximized")
         options.add_argument("--window-size=1920,1080")
