584eca0db719b66caf5be046f7ad1fe5ab74cf15
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-09-01 15:25:44 -0300

    add files via upload

--- app.py
+++ app.py
@@ -16,14 +16,6 @@ email = "arquimarsx@gmail.com"
 password = "szgcbdzxgjkzggbq"
 group_names = ['AMISTADES Y OTROS', 'ENTRA', 'ENTREN GUAPOS', 'ENTRA TE ESPERO :)', 'VIRTUALITOS', 'AMISTADES SUDAMERICA', 'ENTRA AMOR', 'ENTRA AQUI :)', 'ENTREN ENTREN', 'VIRTUALITOS 2024']
 group_link = "https://chat.whatsapp.com/KbrxPxeqIDCHUUdYUoPJMG"
-host = "74.119.144.60"
-port = "4145"
-# 189.240.60.164:9090 mx
-# 189.240.60.169:9090 mx
-# 200.174.198.86:8888 br
-# 172.233.155.25:1080 us socks5
-# 199.229.254.129:4145 us socks5
-# 74.119.144.60:4145 us socks5
 
 def automate():
     try:
@@ -51,25 +43,6 @@ def automate():
         dt_string = dtime.strftime("%d/%m/%Y %H:%M:%S")
         names = random.choice(group_names)
         print("Automação iniciada! - ", dt_string)
-        driver.get("chrome-extension://iejkjpdckomcjdhmkemlfdapjodcpgih/data/popup/popup.html")
-        print("55")
-        time.sleep(5)
-        driver.find_element(By.XPATH,'/html/body/div/details[5]/summary').click()
-        print("58")
-        #driver.find_element(By.XPATH,'//*[@id="socks5-scheme"]').click()
-        protocol_bt = driver.find_element(By.XPATH,'//*[@id="socks5-scheme"]')
-        print("61")
-        ActionChains(driver).move_to_element(protocol_bt).click(protocol_bt).perform()
-        print("63")
-        driver.find_element(By.XPATH,'//*[@id="http-host"]').send_keys(host)
-        print("65")
-        driver.find_element(By.XPATH,'//*[@id="http-port"]').send_keys(port)
-        print("67")
-        driver.find_element(By.XPATH,'//*[@id="single"]').click()
-        print("69")
-        driver.find_element(By.XPATH,'/html/body/div/details[5]/table/tbody/tr[1]/td[1]').click()
-        print("71")
-        time.sleep(15)
         driver.get("https://www.gruposwats.com")
         driver.find_element(By.XPATH, '//*[@id="btnpublica"]').click()
         driver.find_element(By.XPATH, '//*[@id="myDiv3"]/span[6]').click()
