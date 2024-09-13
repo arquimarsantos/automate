77845dac0c7fb700d0dd00048d8463da7dc58ee9
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-09-13 16:44:57 -0300

    add files via upload

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import random
 from datetime import datetime
 
 #app = Flask(__name__)
-email = "xarquis99gmail.com"
+email = "arquimardltr@gmail.com"
 #password = "szgcbdzxgjkzggbq"
 password = "visdbhphohxhmxhj"
 group_names = ['AMISTADES Y OTROS', 'ENTRA', 'ENTREN GUAPOS', 'ENTRA TE ESPERO', 'ENTRA ESTOY ABURRIDA', 'AMISTADES SUDAMERICA', 'ENTRA AMOR', 'ENTRA AQUI', 'ENTREN ENTREN', 'ENTRA Y GANA UN REGALO', 'CHISMEAR DE LA VIDA', 'CHISMEAR Y MÁS', 'Entren mis bros', 'Entren mis latinos', 'Estoy aburrida entren']
