85b4606d19161a0def68ec161f34c4a9abe4c8ff
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-09-14 03:23:52 -0300

    add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ group_link = "https://chat.whatsapp.com/C4fTvmlM5Rt4Z30XGRTNrk"
 def automate():
     try:
         options = webdriver.ChromeOptions()
-        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.1 Safari/537.36'
         options.add_argument(f'user-agent={user_agent}')
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
